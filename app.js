33306bb509bdee116c44c2018c4ef26c97fb6e86
Antonio Gonzalez <goantonio31@outlook.com>
2021-05-13 17:32:00 -0700

    updated the secrets get and post requests rendered all secrets to the page

--- app.js
+++ app.js
@@ -43,7 +43,8 @@ const userSchema = new mongoose.Schema ({
 
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -108,6 +109,33 @@ app.get("/auth/facebook", passport.authenticate("facebook", {scope: ['profile']}
 app.get("/auth/facebook/secrets",
   passport.authenticate("facebook", { successRedirect: "/secrets", failureRedirect: "/login" }));
 
+app.get("/submit", function(req, res){
+    if (req.isAuthenticated()){
+        res.render("submit");
+    } else {
+        res.redirect("/login");
+    }
+})
+
+app.post("/submit", function (req, res){
+    const submittedSecret = req.body.secret;
+
+    // console.log(req.user.id);
+
+    User.findById(req.user.id, function(err, foundUser){
+        if (err){
+            console.log(err)
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
+
 app.get("/logout", function(req,res){
     req.logout();
     res.redirect("/");
@@ -123,12 +151,17 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-    if (req.isAuthenticated()){
-        res.render("secrets")
-    }else {
-        res.redirect("/login");
-    }
+    User.find({"secret": {$ne:null}}, function(err, foundUsers){
+        if (err){
+            console.log(err)
+        } else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
 });
+
 
 app.post("/register", function(req, res){
 
